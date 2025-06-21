2e56b1704b6c0fb129c180efb3d68173e7cc2b03
ViKraym <168685754+ViKraym@users.noreply.github.com>
2025-06-21 14:44:35 +0300

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,6 @@
    
-  const hideButton = document.getElementsByClassName('hideButton');
-  const myBlock = document.getElementsByClassName('delivery');
+  const hideButton = document.getElementsById('hideButton');
+  const myBlock = document.getElementsById('delivery');
 
   hideButton.addEventListener('click', function() {
    
